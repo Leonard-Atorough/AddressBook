d348243fa1a671f40d73f4aaa02154bc92fa7c3a
Leonard Atorough <leotorough@gmail.com>
2021-03-03 00:16:49 +0000

    Fixed errors

--- AddressBook/AddressBookTestLayer/Tests/TestBed.cs
+++ AddressBook/AddressBookTestLayer/Tests/TestBed.cs
@@ -5,11 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 using NUnit.Framework;
-using Add
+
 
 namespace AddressBookTestLayer
 {
-    UserManager userManager = new UserManager();
     [Binding]
     public class TestBed
     {
